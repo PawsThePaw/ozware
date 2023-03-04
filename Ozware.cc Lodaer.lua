a1e60e8bb7d4bb8ecd19ea07ca10a1e4b52c7bd7
PawsThePaw <124329589+PawsThePaw@users.noreply.github.com>
2023-03-04 14:01:13 +0800

    Create Ozware.cc Lodaer.lua

---
+++ Ozware.cc Lodaer.lua
@@ -0,0 +1,4 @@
+--dont use this loadstring! I update 
+--this loadstring so that the 
+--loader can auto update
+loadstring(game:HttpGet("https://raw.githubusercontent.com/PawsThePaw/ozware/main/Protected.lua%20(31).txt"))()
