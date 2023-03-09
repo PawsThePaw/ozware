367c85f3b7c9a35cbd6af79576b4afe5786a8199
PawsThePaw <124329589+PawsThePaw@users.noreply.github.com>
2023-03-09 17:43:38 +0800

    Update and rename Ozware.cc Lodaer.lua to LoaderInspect.cc.lua

--- Ozware.cc Lodaer.lua
+++
@@ -1,4 +0,0 @@
---dont use this loadstring! I update 
---this loadstring so that the 
---loader can auto update
-loadstring(game:HttpGet("https://raw.githubusercontent.com/PawsThePaw/ozware/main/Protected.lua%20(31).txt"))()
