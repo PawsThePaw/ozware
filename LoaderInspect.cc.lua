401bbba1dcd4a24e99ab3e3d8e8df0f3f3cc54db
PawsThePaw <124329589+PawsThePaw@users.noreply.github.com>
2023-03-12 15:41:09 +0800

    Update LoaderInspect.cc.lua

--- LoaderInspect.cc.lua
+++ LoaderInspect.cc.lua
@@ -1,4 +1,4 @@
 --dont use this loadstring! I update 
 --this loadstring so that the 
 --loader can auto update
-loadstring(game:HttpGet("https://raw.githubusercontent.com/PawsThePaw/ozware/main/0.0.4.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/PawsThePaw/ozware/main/0.0.5.txt"))()
